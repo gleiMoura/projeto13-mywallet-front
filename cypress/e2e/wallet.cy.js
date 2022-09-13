22860963bbdfcda9e9d22818b4ab0413ec2f6c41
gleiMoura <gle.bra.mou@gmail.com>
2022-09-13 10:09:21 -0300

    add a beforeEach in e2e test file

--- cypress/e2e/wallet.cy.js
+++ cypress/e2e/wallet.cy.js
@@ -2,11 +2,16 @@ import dotenv from "dotenv";
 import client from "../../factories/walletFactorie";
 dotenv.config();
 
+beforeEach(() => {
+	cy.visit('http://localhost:3001')
+});
+
 describe("Login page", () => {
 	it('Do login', async () => {
-		cy.visit('http://localhost:3001')
 		cy.get('#email').type(client.name)
-		cy.get('#password').type(client.password)
+		cy.get('#password').type(`${client.password}`)
+		cy.intercept('POST', 'https://mywallet.onrender.com/signIn').as('signin')
 		cy.get('form > :nth-child(3)').click()
+		cy.wait('@signin')
 	})
 })
